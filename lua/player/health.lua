54254bab65c00648629d58a537216f7b67d3e606
alexxGmZ <algilbertgomez@gmail.com>
2024-11-26 12:15:38 +0800

    fix: compare vim.fn.executable("playerctl") to 0

--- lua/player/health.lua
+++ lua/player/health.lua
@@ -3,7 +3,7 @@ local M = {}
 ---Checkhealth
 function M.check()
    vim.health.start("Check requirements")
-   if not vim.fn.executable('playerctl') then
+   if vim.fn.executable('playerctl') == 0 then
       vim.health.warn("playerctl is not installed or can not be found in PATH",
          "Please install playerctl and make sure the command `playerctl` is executable to use this plugin")
    else
